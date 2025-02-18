f60fef7379852176c7cf75e6738a0a6a3e709112
neerajbhasin-8055 <bassanneeraj@gmail.com>
2025-02-18 20:41:12 +0530

    Company logic and company routes are done

--- backend/models/company.model.js
+++ backend/models/company.model.js
@@ -1,4 +1,4 @@
-import mongoose, { mongo } from "mongoose";  
+import mongoose from "mongoose";  
 
 const companySchema = new mongoose.Schema({
     name:{
---
+++ backend/routes/company.route.js
@@ -0,0 +1,12 @@
+import express from "express"
+
+import isAuthenticated from "../middlewares/isAuthenticated.js"
+import { getCompany, getCompanyById, registerComapny, updateCompany } from "../controllers/company.controller.js"
+const router = express.Router()
+
+router.route('/register').post(isAuthenticated,registerComapny)
+router.route('/getCompany').get(isAuthenticated,getCompany)
+router.route('/getCompany/:id').get(isAuthenticated,getCompanyById)
+router.route('/update/:id').put(isAuthenticated,updateCompany)
+
+export default router
